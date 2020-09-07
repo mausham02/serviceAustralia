91006ca5547ab89216dfa48cd810e7f054ad65c8
Mausham Shrestha <mausham.shrestha.01@gmail.com>
2020-09-08 01:10:58 +1000

    Finishing touch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const jwt=require('jsonwebtoken')
 
+
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
 // Adding middle ware to connect frontend and backend
@@ -8,7 +9,21 @@ const cors=require('cors')
 const path=require('path')
 
 const app=express()
-app.use(cors())
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 app.use(bodyParser.json())
 
 
